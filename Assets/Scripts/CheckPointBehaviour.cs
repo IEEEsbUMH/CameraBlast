e2786952942828f6dd0cb0fdb305c1ae29bde939
Héctor Barreras Almarcha <hbaelx@gmail.com>
2013-11-30 18:10:14 +0100

    - Checkpoint prefab created and added some related scripts (half done)

---
+++ Assets/Scripts/CheckPointBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointBehaviour : MonoBehaviour
+{
+	
+		void OnTriggerEnter (Collider a_collider)
+		{
+				//Is it the player?
+				print (0);
+				if (a_collider.gameObject.tag == Tags.PLAYER) {
+						a_collider.gameObject.GetComponent<PlayerSpawning> ().replaceSpawn (transform.gameObject);
+				}
+		}
+}
---
+++ Assets/Scripts/Player/PlayerSpawning.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawning : MonoBehaviour
+{
+
+		public GameObject lastCheckpoint;
+
+		void Respawn ()
+		{
+
+		}
+
+		public void replaceSpawn (GameObject a_spawn)
+		{
+				lastCheckpoint = a_spawn;
+		}
+}
