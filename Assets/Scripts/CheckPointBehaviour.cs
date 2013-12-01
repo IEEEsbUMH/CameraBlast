f841074c8ebbb1ab439b3072b58e2bae497e34ad
Héctor Barreras Almarcha <hbaelx@gmail.com>
2013-12-01 22:44:18 +0100

    - Player no longer floats over the floor - Player is now able to jump and use the jetpack - Added a couple cameras to the scene - Added some more checkpoints to the scene and tweaked the behavior a bit - Turret prefab added - Light changed

--- Assets/Scripts/CheckPointBehaviour.cs
+++ Assets/Scripts/CheckPointBehaviour.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class CheckPointBehaviour : MonoBehaviour
 {
-	
+		public float gravityMultiplierAfterUsing;
+		public Color startColorAfterUsing;
+
 		void OnTriggerEnter (Collider a_collider)
 		{
 				//Is it the player?
-				print (0);
 				if (a_collider.gameObject.tag == Tags.PLAYER) {
 						a_collider.gameObject.GetComponent<PlayerSpawning> ().replaceSpawn (transform.gameObject);
+
+						//particleSystem.gravityModifier = gravityMultiplierAfterUsing;
+						//particleSystem.startColor = startColorAfterUsing;
+						particleSystem.loop = false;
 				}
 		}
 }
--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -7,13 +7,20 @@ public class CharacterMovement : MonoBehaviour
 {
 
 		public bool controlIsActive;
+		public bool jetpackIsAvailable;
 		public GameObject POV;
 		public int mouseSensitivity;
 		public float movementSpeed;
+		public float jumpPower;
+		public float jetpackPower;
 		public int minHeadAngle;
 		public int maxHeadAngle;
 		protected Vector3 pr_movementDirection;
 		protected Vector3 pr_cameraDirection;
+		protected float pr_jumpValue;
+		protected float pr_jetpackValue;
+		protected bool pr_grounded; //Will be true if isGrounded hasn't been false during the last 5 FixedUpdate calls
+		protected int h_notGroundedCount;
 
 		protected CharacterController pr_controller;
 
@@ -25,11 +32,16 @@ public class CharacterMovement : MonoBehaviour
 		{
 				pr_controller = GetComponent<CharacterController> ();
 				pr_movementDirection = new Vector3 ();
+				pr_jumpValue = 0;
+				pr_jetpackValue = 0;
+				pr_grounded = false;
+				h_notGroundedCount = 0;
 		}
 	
 		// Update is called once per frame
 		void FixedUpdate ()
 		{
+				checkGrounding ();
 				processInput ();
 				processMovement ();
 		}
@@ -44,29 +56,49 @@ public class CharacterMovement : MonoBehaviour
 
 				transform.Rotate (0, Input.GetAxis ("Mouse X") * mouseSensitivity * Time.deltaTime, 0);
 				
-				
 
 				//Player movement
 				//Only overwritten if the player is on the floor
-				if (Mathf.Abs (pr_controller.velocity.y) < 2) {
+				if (pr_grounded) {
 						pr_movementDirection.x = Input.GetAxis ("Horizontal");
 						pr_movementDirection.z = Input.GetAxis ("Vertical");
 						//pr_movementDirection.Normalize ();
 						pr_movementDirection *= movementSpeed * Time.deltaTime;
 				}
+
+				pr_jumpValue = Input.GetAxis ("Jump");
+
+				if (jetpackIsAvailable) {
+						pr_jetpackValue = Input.GetAxis ("Jetpack");
+				}
 		}
 
 		void processMovement ()
 		{
-				if (pr_controller.isGrounded) {
-						pr_speed = 0;
+				if (pr_grounded) {
+						pr_speed = pr_jumpValue > 0 ? jumpPower : 0;
 				} else {
 						//Add gravity
 						pr_speed += Physics.gravity.y * Time.deltaTime;	
+				}
+
+				//Modifies pr_speed if jetpack is working
+				if (pr_jetpackValue > 0) {
+						pr_speed += jetpackPower;
 				}
 				
 				Vector3 t_gravityMovement = new Vector3 (0, pr_speed * Time.deltaTime, 0);
 
 				pr_controller.Move (transform.right * pr_movementDirection.x + transform.forward * pr_movementDirection.z + t_gravityMovement);
+		}
+
+		void checkGrounding ()
+		{
+				if (pr_controller.isGrounded) {
+						pr_grounded = true;
+						h_notGroundedCount = 0;
+				} else if (++h_notGroundedCount < 5) {
+						pr_grounded = false;
+				}
 		}
 }
