73e7425de8aea0a99986160d57b11fef5153df5f
Héctor Barreras Almarcha <hbaelx@gmail.com>
2013-12-02 00:46:46 +0100

    - Advances in level design - Added some sounds to the library - Added moving platforms

---
+++ Assets/Scripts/MovingPlatformBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformBehaviour : MonoBehaviour
+{
+
+		public float StartX;
+		public float RelativeEndX;
+		public bool takePositionAsStartX;
+		public float MoveSpeed;
+		public float defaultToTargetValue;
+		protected float pr_targetX;
+		protected float pr_endX;
+
+		void Start ()
+		{
+				if (takePositionAsStartX) {
+						StartX = transform.position.x;
+				} else {
+						transform.position.Set (StartX, transform.position.y, transform.position.z);
+				}
+
+				pr_endX = StartX + RelativeEndX;
+
+				pr_targetX = pr_endX;
+		}
+
+		void FixedUpdate ()
+		{
+				//Checks if the distance to the target is less than the wanted to default
+				if (Mathf.Abs (transform.position.x - pr_targetX) <= defaultToTargetValue) {
+						transform.position.Set (pr_targetX, transform.position.y, transform.position.z);
+						pr_targetX = pr_targetX == StartX ? pr_endX : StartX;
+				}
+
+				if (transform.position.x < pr_targetX) {
+						transform.Translate (MoveSpeed * Time.deltaTime, 0, 0);
+				}
+
+				if (transform.position.x > pr_targetX) {
+						transform.Translate (-MoveSpeed * Time.deltaTime, 0, 0);
+				}
+		}
+}
