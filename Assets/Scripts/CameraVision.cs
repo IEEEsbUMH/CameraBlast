2fa663f6318bf255f5b8216f7e88d7da0412fbd9
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-12-08 21:02:27 +0100

    - Implemented CameraVision Script.

---
+++ Assets/Scripts/CameraVision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraVision : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 forward;
+	private Vector3 targetDirection;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetDirection = player.transform.position - transform.position;
+		forward = transform.forward;
+		float angle = Vector3.Angle(targetDirection, forward);
+		if(angle < 25.0F){
+			FollowPlayer.isInRange = true;
+			CameraRotation.isInRange = true;
+		}
+	}
+}
