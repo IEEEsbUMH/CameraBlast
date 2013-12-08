5ac40d749548c7b2da22edc4a2abe1ac12ce2de1
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-12-08 18:03:58 +0100

    - Implemented FollowPlayer movement on Camera.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject Player;
+
+	public static bool isInRange;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isInRange) {
+			transform.LookAt (Player.transform);
+			GetComponentInChildren<Light> ().light.color = Color.red;
+		} else {
+			GetComponentInChildren<Light> ().light.color = Color.green;
+		}
+	}
+}
