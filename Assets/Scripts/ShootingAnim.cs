d3746112f6e96750c2465bd7de69ee929bf1b7a7
Luis Fletcher Ortega <luis.fletcher.ortega@gmail.com>
2013-12-11 23:26:22 +0100

    -Turret sound mode edited.

--- Assets/Scripts/ShootingAnim.cs
+++ Assets/Scripts/ShootingAnim.cs
@@ -8,15 +8,11 @@ public class ShootingAnim : MonoBehaviour {
 	public int speedrotation; //ALWAYS set to 1.
 	public int accrotation; //ALWAYS set to 10.
 	public bool activateshooting;
-	public AudioSource shootingsound;
-	public AudioSource shootingstart;
-	public AudioSource shootingfinish;
+	public AudioSource turretsound;
+	public AudioClip shootingsound;
+	public AudioClip shootingstart;
+	public AudioClip shootingfinish;
 
-	void Start () 
-	{
-
-	}
-	
 	void Update () 
 	{
 		if (activateshooting) {
@@ -24,24 +20,28 @@ public class ShootingAnim : MonoBehaviour {
 			if (speedrotation < 900) {
 				speedrotation += accrotation;
 				Shooting ();
-				if(shootingstart.isPlaying==false){
-					shootingstart.Play(30000);
-					shootingstart.pitch=0.5f;
+				turretsound.clip=shootingstart;
+				if(turretsound.isPlaying==false){
+					turretsound.pitch=0.4f;
+					turretsound.Play(25000);
 				}
 			} else {
 				Shooting ();
-				shootingstart.Stop();
-				if(shootingsound.isPlaying==false){
-					shootingsound.Play();
+				turretsound.clip=shootingsound;
+				turretsound.pitch=1;
+				turretsound.loop=false;
+				if(turretsound.isPlaying==false){
+					turretsound.Play();
 				}
 			}
 		} else {
-			shootingsound.Stop();
 			if(speedrotation>0){
 				speedrotation-=accrotation;
 				Shooting();
-				if(speedrotation>600 && shootingfinish.isPlaying==false){
-					shootingfinish.Play();
+				turretsound.clip=shootingfinish;
+				if(speedrotation>600 && turretsound.isPlaying==false){
+					turretsound.pitch=0.4f;
+					turretsound.Play();
 				}
 			}else{
 				speedrotation=0;
