99a97f2ba4128cdabb68ad9162bf009450413111
Héctor Barreras Almarcha <hbaelx@gmail.com>
2013-12-14 17:05:31 +0100

    - New texture applied to the floor - Changed Play(delay) for PlayDelayed - Raycasting now determines the length of the bullet trails for turret

---
+++ Assets/Scripts/LengthBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LengthBehaviour : MonoBehaviour
+{
+
+		protected Ray pr_ray;
+		protected RaycastHit pr_rayHit = new RaycastHit ();
+
+		// Use this for initialization
+		void Start ()
+		{
+				pr_ray = new Ray (transform.position, Vector3.left);
+		}
+	
+		// Update is called once per frame
+		public void recalculate ()
+		{
+				if (Physics.Raycast (pr_ray, out pr_rayHit, 10)) {
+						transform.localScale = new Vector3 (pr_rayHit.distance, 1, 1);
+				}
+		}
+}
--- Assets/Scripts/ShootingAnim.cs
+++ Assets/Scripts/ShootingAnim.cs
@@ -2,100 +2,99 @@
 using UnityEngine;
 using System.Collections;
 
-public class ShootingAnim : MonoBehaviour {
+public class ShootingAnim : MonoBehaviour
+{
 	
-	public GameObject Cannons;
-	public int SpeedRotation; //ALWAYS set to 1.
-	public int AccRotation; //ALWAYS set to 10.
-	public bool ActivateShooting;
-	public AudioSource TurretSound;
-	public AudioClip ShootingSound;
-	public AudioClip ShootingStart;
-	public AudioClip ShootingFinish;
-	public GameObject ShootingLine;
-	protected int FrameCounter;
-	private GameObject player;
-	public int rangeTurret;
+		public GameObject Cannons;
+		public int SpeedRotation; //ALWAYS set to 1.
+		public int AccRotation; //ALWAYS set to 10.
+		public bool ActivateShooting;
+		public AudioSource TurretSound;
+		public AudioClip ShootingSound;
+		public AudioClip ShootingStart;
+		public AudioClip ShootingFinish;
+		public GameObject ShootingLine;
+		protected int FrameCounter;
+		private GameObject player;
+		public int rangeTurret;
 
 
-	void Start(){
-		SpeedRotation = 0;
-		AccRotation = 10;
-		ShootingLine.GetComponent<LineRenderer>().enabled = false;
-		FrameCounter = 0;
-		player = GameObject.FindGameObjectWithTag ("Player");
-	}
+		void Start ()
+		{
+				SpeedRotation = 0;
+				AccRotation = 10;
+				ShootingLine.GetComponent<LineRenderer> ().enabled = false;
+				FrameCounter = 0;
+				player = GameObject.FindGameObjectWithTag ("Player");
+		}
 
-	void Update () 
-	{
-		float distance = Vector3.Distance (transform.position, player.transform.position);
-		if (CameraRotation.isInRange && distance < rangeTurret) {
+		void Update ()
+		{
+				float distance = Vector3.Distance (transform.position, player.transform.position);
+				if (CameraRotation.isInRange && distance < rangeTurret) {
 						ActivateShooting = true;
 				} else {
 						ActivateShooting = false;
 				}
-		if (ActivateShooting) {
+				if (ActivateShooting) {
 
-			if (SpeedRotation < 900) {
-				SpeedRotation += AccRotation;
-				Shooting ();
-				TurretSound.clip=ShootingStart;
-				if(TurretSound.isPlaying==false){
-					TurretSound.pitch=0.4f;
-					TurretSound.Play(25000);
+						if (SpeedRotation < 900) {
+								SpeedRotation += AccRotation;
+								Shooting ();
+								TurretSound.clip = ShootingStart;
+								if (TurretSound.isPlaying == false) {
+										TurretSound.pitch = 0.4f;
+										TurretSound.PlayDelayed (25000);
+								}
+						} else {
+								Shooting ();
+								TurretSound.clip = ShootingSound;
+								TurretSound.pitch = 1;
+								TurretSound.loop = false;
+								if (TurretSound.isPlaying == false) {
+										TurretSound.Play ();
+								}
+						}
+				} else {
+						if (SpeedRotation > 0) {
+								ShootingLine.GetComponent<LineRenderer> ().enabled = false;
+								SpeedRotation -= AccRotation;
+								Shooting ();
+								TurretSound.clip = ShootingFinish;
+								if (SpeedRotation > 600 && TurretSound.isPlaying == false) {
+										TurretSound.pitch = 0.4f;
+										TurretSound.Play ();
+								}
+						} else {
+								SpeedRotation = 0;
+						}
 				}
-			} else {
-				Shooting ();
-				TurretSound.clip=ShootingSound;
-				TurretSound.pitch=1;
-				TurretSound.loop=false;
-				if(TurretSound.isPlaying==false){
-					TurretSound.Play();
-				}
-			}
-		} else {
-			if(SpeedRotation>0){
-				ShootingLine.GetComponent<LineRenderer>().enabled= false;
-				SpeedRotation-=AccRotation;
-				Shooting();
-				TurretSound.clip=ShootingFinish;
-				if(SpeedRotation>600 && TurretSound.isPlaying==false){
-					TurretSound.pitch=0.4f;
-					TurretSound.Play();
-				}
-			}else{
-				SpeedRotation=0;
-			}
-		}
 				
-	}
-
-	void FixedUpdate()
-	{
-		if (ActivateShooting && SpeedRotation==900)
-		{
-			FrameCounter++;
-			if(FrameCounter<5)
-			{
-				ShootingLine.GetComponent<LineRenderer>().enabled= true;
-			}
-			if(FrameCounter>5)
-			{
-				ShootingLine.GetComponent<LineRenderer>().enabled= false;
-			}
-			if(FrameCounter==10)
-			{
-				FrameCounter=0;
-			}
 		}
 
-	}
+		void FixedUpdate ()
+		{
+				if (ActivateShooting && SpeedRotation == 900) {
+						FrameCounter++;
+						if (FrameCounter < 5) {
+								ShootingLine.GetComponent<LineRenderer> ().enabled = true;
+								ShootingLine.GetComponent<LengthBehaviour> ().recalculate ();
+						}
+						if (FrameCounter > 5) {
+								ShootingLine.GetComponent<LineRenderer> ().enabled = false;
+						}
+						if (FrameCounter == 10) {
+								FrameCounter = 0;
+						}
+				}
+
+		}
 
 
-	void Shooting ()
-	{
-		transform.Rotate (0, 0, Time.deltaTime * SpeedRotation);
+		void Shooting ()
+		{
+				transform.Rotate (0, 0, Time.deltaTime * SpeedRotation);
 
-	}
+		}
 }
 
