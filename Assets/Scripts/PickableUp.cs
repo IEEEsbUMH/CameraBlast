1c5eb895d9eaa6fc08b5570a4ade119f49aa0ba8
Héctor Barreras Almarcha <hbaelx@gmail.com>
2013-12-17 01:38:10 +0100

    - Door added to stage - Some scripts for pickup started - Sound source for weapon added

---
+++ Assets/Scripts/PickableUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickableUp : MonoBehaviour
+{
+		public int RotationSpeed;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				transform.Rotate (0, RotationSpeed * Time.deltaTime, 0);
+		}
+
+		void OnTriggerEnter (Collider a_collider)
+		{
+
+		}
+}
